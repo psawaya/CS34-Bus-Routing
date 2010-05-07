797c55522c601b73f5edc9f479e6bc62c7f6978a
John M. Schanck <john@anomos.info>
2010-05-07 05:10:51 -0400

    Proper input files. getopt style argument parsing

--- Python Router/TSPSolve/MultiTour.py
+++ Python Router/TSPSolve/MultiTour.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python2.6
+
 from parser import Parser
 from Tour import Tour
 
@@ -7,82 +9,87 @@ import json #not using python's built-in json to preserve compatibility with pyt
 
 import re
 
+import sys
+
 class MultiTour:
-    def __init__(self):
+    def __init__(self, mapinfo):
         self.routes = []
         self.tours = []
-        
+
+        self.mapinfo = mapinfo
         self.addresses = []
         self.coordinates = []
-        
+        for i in self.mapinfo.addresses:
+            a,c = i.split("|")
+            self.addresses.append(a)
+            self.coordinates.append(map(float, c.strip("() ").split(",")))
+
         self.routes_json = None
-        
-        self.tourBase = ["",None]
-        
-        self.mapinfo = None
+
+        self.tourBase = [self.addresses[0],self.coordinates[0]]
+
+        self.buildTours()
 
     def calculateScore(self):
         return sum([tour.score for tour in self.tours])
-    
+
     # Writes tour data to JSON file, suitable for Google Maps integration
-    def dumpToFile(self,filename):        
-        coordinatesRegex = re.compile("\(([0-9\-\.]{1,})\,\s*?([0-9\-\.]{1,})\)")
-        
+    def dumpToFile(self,filename):
         tourRoutes = []
         for tour in self.tours:
             tourCoordinates = []
 
             for node in tour.tour:#tour.tourToIndices():
-                coordinates = coordinatesRegex.match(self.coordinates[node]).groups()
-                tourCoordinates.append([node, [float(coordinates[0]),float(coordinates[1])]])
-            
+                tourCoordinates.append([node, list(self.coordinates[node])])
+
             tourRoutes.append(tourCoordinates)
-            
+
         f = open(filename,'w')
-        
-        f.write(json.write({'base' : {'name': self.tourBase[0], 'coords' : self.parseCoordinates(self.coordinates[self.tourBase[1]])},
+
+        f.write(json.write({'base' : {'name': self.tourBase[0], 'coords' : self.tourBase[1]},
                 'routes' : tourRoutes, 'routeLength' : self.calculateScore()}))
-        
+
         f.close()
-    
+
     def dumpToDOTFile(self,filename):
         f = open(filename,'w')
-        
+
         f.write("graph G {\n")
-        
-        for node in self.addresses.iterkeys():
-            coordinates = self.parseCoordinates(self.coordinates[self.addresses[node]])
-            f.write("n%s [ pos = \"%s,%s!\"];\n" % (self.addresses[node],abs(float(coordinates[0])-int(coordinates[0]))*200,abs(float(coordinates[1])-int(coordinates[1]))*200))
-        
+
+        for node in range(len(self.addresses)):
+            coordinates = self.coordinates[node]
+            f.write("n%s [ pos = \"%s,%s!\"];\n" %
+                    (self.addresses[node],abs(coordinates[0] % 1)*200,abs(coordinates[1] % 1)*200))
+
         for tour in self.tours:
-            for node in self.getRoutePairs(tour):                
+            for node in self.getRoutePairs(tour):
                 f.write("n%s -- n%s;\n" % node)
-        
+
         f.write("\n}")
         f.close()
-        
+
     def dumpDiffsToDOTFile(self,filename,diffs):
         f = open(filename,'w')
-        
+
         f.write("graph G {\n")
-        
-        for node in self.addresses.iterkeys():
-            coordinates = self.parseCoordinates(self.coordinates[self.addresses[node]])
+
+        for node in range(len(self.addresses)):
+            coordinates = self.coordinates[node]
             f.write("n%s [label=\"\",size=\"1\"];\n" % (self.addresses[node]))
             #,abs(int(coordinates[1]*10) - float(coordinates[1]*10))*300,abs(int(coordinates[0]*10) - float(coordinates[0]*10))*300)
             # f.write("n%s [ pos = \"%s,%s!\"];\n" % (self.addresses[node],abs(float(coordinates[0])-int(coordinates[0]))*200,abs(float(coordinates[1])-int(coordinates[1]))*200))
-            
-        
+
+
         for tourIdx in range(len(self.tours)):
             routePairs = self.getRoutePairs(self.tours[tourIdx])
             edgeList = []
-            
+
             for pair in routePairs:
                 edgeList.append((pair[0],pair[1]))
                 edgeList.append((pair[1],pair[0]))
-                
+
             print edgeList
-            
+
             for nodeIdx in range(len(routePairs)):
                 if diffs[tourIdx][nodeIdx] not in edgeList:
                     f.write("n%s -- n%s [color=\"#ff0000\"];\n" % routePairs[nodeIdx])
@@ -91,7 +98,7 @@ class MultiTour:
                     f.write("n%s -- n%s [label=\"%s m\"];\n" % (routePairs[nodeIdx][0],routePairs[nodeIdx][1],self.mapinfo.getCost(*routePairs[nodeIdx])))
         f.write("\n}")
         f.close()
-    
+
     def swapBetweenRoutes(self,probabilityOfWorsening=0):
         pass
         # routeIdxFrom = random.sample(range(len(self.tours)),1)[0]
@@ -115,56 +122,56 @@ class MultiTour:
         #     insertIdx = closestInNewRoute[0]
             #TODO:  Generate all permutations of area around insertIdx
         #     
-        
+
         # print "mapinfo: %s" % closest
         # print "compare: %s " %          self.tours[routeIdxTo].returnClosestToNode(randomNode)
         # self.tours[routeIdxTo].GENI(randomNode)
-        
-        
+
+
 
     def getRoutePairs(self,tour):
         return [(tour.tour[x],tour.tour[x+1]) for x in range(len(tour.tour)-1)]
 
     def readRoutes(self,filename):
         f = open(filename)
-        
+
         routesTxt = f.read()
-        
+
         f.close()
-        
+
         self.routes_json = json.read(routesTxt)
-    
-    def buildTours(self,mapinfo):
-        alltour = set()
-        map(alltour.update, self.routes)
-        self.tours = [Tour(mapinfo, names=list(alltour), default_tour=list(alltour))]
+
+    def buildTours(self):
+        #alltour = set()
+        #map(alltour.update, self.routes)
+        allt = range(len(self.addresses))
+        best = self.mapinfo.best_known or allt[:]
+        print allt, best
+
+        self.tours = [Tour(self.mapinfo, names=allt, default_tour=best)]
         self.allstops = self.tours[0]
 
-        self.mapinfo = mapinfo
         #for route in self.routes:
-        #    newTour = Tour(mapinfo, names=route, default_tour=route)
+        #    newTour = Tour(self.mapinfo, names=route, default_tour=route)
         #    self.tours.append(newTour)
 
         #print self.tours
 
     #HACKFEST 2k10
-    def correlateWithAddresses(self,addressesFilename):
-        self.addresses = dict()  #Unfortunately, we're doing this by index,
-                        # so build a dict of addresses->indices
-        
-        f = open (addressesFilename)
-        
-        fileLines = f.readlines()
-        
-        f.close()
-        
-        for lineIdx in range(len(fileLines)):
-            line = fileLines[lineIdx]
-            justAddressIdx = line.find("(")
-            self.addresses[line[0:justAddressIdx].strip()] = lineIdx
+    def correlateWithAddresses(self,addressesFilename=None):
+        if self.mapinfo.addresses is None or addressesFilename is not None:
+            self.addresses = dict()  #Unfortunately, we're doing this by index,
+                                     # so build a dict of addresses->indices
+            f = open (addressesFilename)
+            fileLines = f.readlines()
+            f.close()
 
-            self.coordinates.append(line[justAddressIdx:].strip()) #save coordinates for file dump
-        print self.addresses
+            for lineIdx in range(len(fileLines)):
+                line = fileLines[lineIdx]
+                justAddressIdx = line.find("(")
+                self.addresses[line[0:justAddressIdx].strip()] = lineIdx
+
+                self.coordinates.append(line[justAddressIdx:].strip()) #save coordinates for file dump
 
         for routeIdx in range(len(self.routes_json['stops'])):
             self.routes.append([])
@@ -174,7 +181,7 @@ class MultiTour:
                 except KeyError:
                     #try no replacement.
                     self.routes[routeIdx].append (self.addresses[self.standardizeAddress(address,False).strip()])
-                
+
         self.tourBase = [self.routes_json['home'],self.addresses[self.standardizeAddress(self.routes_json['home'])]]
 
         # print self.coordinates
@@ -188,30 +195,15 @@ class MultiTour:
         exp = tagd[:len(self.routes)]
 
     @staticmethod
-    def all_perms(str):
-        if len(str) <=1:
-            yield str
-        else:
-            for perm in all_perms(str[1:]):
-                for i in range(len(perm)+1):
-                    yield perm[:i] + str[0:1] + perm[i:]    
-
-    @staticmethod
-    def parseCoordinates(coordinatesStr):
-        coordinatesRegex = re.compile("\(([0-9\-\.]{1,})\,\s*?([0-9\-\.]{1,})\)")
-        
-        coordinates = coordinatesRegex.match(coordinatesStr).groups()
-        return [float(coordinates[0]),float(coordinates[1])]
-
-    @staticmethod
     def standardizeAddress(txt,replace=True):
-        print "txt = %s" % txt
-        if (type(txt) == 'list'): txt=txt[0]
+        #print "txt = %s" % txt
+        if (type(txt) == 'list'):
+            txt=txt[0]
         newTxt = txt.lower().strip()
-        
+
         if replace:
             newTxt = newTxt.replace("lane","ln")
-        
+
         if newTxt.find(" pelham, ma 01002") == -1:
             newTxt += " amherst, ma 01002"
 
@@ -220,29 +212,45 @@ class MultiTour:
 
         return newTxt
 
+def printUsage():
+    print "Usage: python %s [--k k-opt degree] [--routes file] [--addresses file] inputfile" % sys.argv[0]
+
 if __name__ == "__main__":
-    multiTour = MultiTour()
+    import getopt
+    if len(sys.argv) == 1:
+        printUsage()
+        sys.exit(1)
 
-    multiTour.readRoutes('routesData/hsStops.json')#('routesData/crockerStops.json')
-
-    multiTour.correlateWithAddresses("../../routes/by_school/high-juniorhigh.txt")
+    optlist, args = getopt.getopt(sys.argv[1:], '', ['routes', 'addresses', 'k='])
+    print optlist, args
+    if len(args) > 1:
+        printUsage()
+        sys.exit(1)
 
     parser = Parser()
-    mapinfo = parser.parse("routesData/hsMatrix.txt")
+    mapinfo = parser.parse(args[0])
 
-    multiTour.buildTours(mapinfo)
+    multiTour = MultiTour(mapinfo)
+
+    opts = dict(optlist)
+    if opts.has_key('addresses'):
+        multiTour.correlateWithAddresses(opts['addresses'])
+    if opts.has_key('routes'):
+        multiTour.readRoutes(opts['routes'])
+    if opts.has_key('k'):
+        k = opts['k']
+    else:
+        k = 3
 
     multiTour.dumpToFile("before.json")
     multiTour.dumpToDOTFile("before.dot")
-    
+
     beforeRoutes = [multiTour.getRoutePairs(tour) for tour in multiTour.tours]
 
     print "initial overall score: %i" % multiTour.calculateScore()
     # multiTour.swapBetweenRoutes()
-    
-    try:
-       print "initial overall score: %i" % multiTour.calculateScore()
 
+    try:
        #for tour in multiTour.tours:
        for tour in multiTour.tours:
            heat = 1.0
@@ -251,15 +259,15 @@ if __name__ == "__main__":
            # while (heat > 0):
            for x in range(count):
                tour.heat = heat
-               tour.annealKOpt(5)
+               tour.annealKOpt(k)
                if heat > 0:
                    heat -= dec
     except KeyboardInterrupt:
         pass
-    
+
     print "final overall score: %i" % multiTour.calculateScore()
     print multiTour.tours
     multiTour.dumpToFile("after.json")
     multiTour.dumpToDOTFile("after.dot")
-    
+
     multiTour.dumpDiffsToDOTFile("diffs.dot",beforeRoutes)
